0982da41268958b24779def91e1249c90830f0f2
Jacob Vejby <jave@c.dk>
2024-05-02 12:34:46 +0200

    incidenceAngleFromEllipsoid selected and hardcoded in split_polarizations

--- pre_process_manager.py
+++ pre_process_manager.py
@@ -16,11 +16,11 @@ class PreProcessor:
         self.wrapper = ResultWrapper(**kwargs)
 
     def start_workflow(self):
-        self.downloader.download_sentinel_1()
+        # self.downloader.download_sentinel_1()
         self.s1_preprocessor.s1_workflow()
 
-        self.downloader.download_sentinel_2()
-        self.s2_preprocessor.s2_workflow()
+        # self.downloader.download_sentinel_2()
+        # self.s2_preprocessor.s2_workflow()
 
         self.wrapper.wrap_results()
 
